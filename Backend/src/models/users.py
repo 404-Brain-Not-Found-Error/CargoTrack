ebdeb7137782d824805804ec307ceb4e6f931697
romaperec <cpser621@gmail.com>
2025-04-01 16:04:52 +0300

    improve registration speed

--- Backend/src/models/users.py
+++ Backend/src/models/users.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
 
 from database import Base
 
@@ -7,5 +8,5 @@ class UserModel(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str]
+    email: Mapped[str] = mapped_column(String, unique=True, index=True)
     password: Mapped[str]
--- Backend/src/utils/hash.py
+++ Backend/src/utils/hash.py
@@ -1,8 +1,13 @@
 from passlib.hash import argon2
+import asyncio
 
+fast_argon2 = argon2.using(
+    time_cost=2,
+    memory_cost=51200,
+)
 
 def hash_password(password: str):
-    return argon2.hash(password)
+    return fast_argon2.hash(password)
 
 
 def check_password(password: str, db_password: str) -> bool:
