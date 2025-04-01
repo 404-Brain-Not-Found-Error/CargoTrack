878db92b10318fe2373d1f897f3a95c8ce44df44
romaperec <cpser621@gmail.com>
2025-04-01 17:43:06 +0300

    add saving jwt in http cookies

--- Backend/src/routes/users.py
+++ Backend/src/routes/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_session
@@ -18,11 +18,21 @@ async def user_registration(
     return await user_service.register_user(user_data.dict())
 
 
-@router.post("/user/login", response_model=TokenResponseSchema)
-async def user_login(user_data: UserSchema, db: AsyncSession = Depends(get_session)):
+@router.post("/user/login")
+async def user_login(user_data: UserSchema, response: Response, db: AsyncSession = Depends(get_session)):
     user_repo = UserRepository(db)
     user_service = UserService(user_repo)
     token = await user_service.login_user(
         email=user_data.email, password=user_data.password
     )
-    return {"access_token": token, "token_type": "bearer"}
+    response.set_cookie(
+        key="access_token",
+        value=token,
+        httponly=True,
+        secure=True,
+        samesite="lax",
+        max_age=3600,
+        path="/",
+    )
+
+    return {"status", "OK"}
