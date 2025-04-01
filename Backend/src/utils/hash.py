265334408448a28e5dcb5bc6c15dd397140e21e0
romaperec <cpser621@gmail.com>
2025-04-01 16:06:55 +0300

    ruff format and check

--- Backend/src/utils/hash.py
+++ Backend/src/utils/hash.py
@@ -1,10 +1,10 @@
 from passlib.hash import argon2
-import asyncio
 
 fast_argon2 = argon2.using(
     time_cost=2,
     memory_cost=51200,
 )
+
 
 def hash_password(password: str):
     return fast_argon2.hash(password)
